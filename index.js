b9aedccf0e5b392a0098dff0b1d257acdeb50343
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-10-06 09:09:11 -0300

    implement monthsAbbr

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict';
 
 var months = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December',
+  'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
+];
+
+var monthsAbbr = [
+  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
 ];
 
 module.exports = {
   months: function() {
     return months;
+  },
+
+  monthsAbbr: function() {
+    return monthsAbbr;
   },
 };
--- test/test.js
+++ test/test.js
@@ -4,8 +4,11 @@ var expect = require('chai').expect;
 var calendar = require('./../index');
 
 var expectedMonths = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December',
+  'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
+];
+
+var expectedMonthsAbbr = [
+  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
 ];
 
 describe('#months', function() {
@@ -20,4 +23,18 @@ describe('#months', function() {
 
     expect(months).to.eql(expectedMonths);
   })
+});
+
+describe('#monthsAbbr', function() {
+  it('should have length of 12', function() {
+    var monthsAbbr = calendar.monthsAbbr();
+
+    expect(monthsAbbr.length).to.equal(12);
+  });
+
+  it('should return all months names abbreviated', function() {
+    var monthsAbbr = calendar.monthsAbbr();
+
+    expect(monthsAbbr).to.eql(expectedMonthsAbbr);
+  });
 });
