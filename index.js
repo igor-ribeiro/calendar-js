ed42d55e3d9b76a5cf10f991db7f30b4b5c09823
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-10-06 08:20:55 -0300

    start developing

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var months = [
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December',
+];
+
+module.exports = {
+  months: function() {
+    return months;
+  },
+};
