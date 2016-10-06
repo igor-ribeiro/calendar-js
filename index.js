00aff3808094a86d0a6bd2035e7f6722679c6090
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-10-06 12:08:38 -0300

    refactor .monthsAbbr to use .months

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ var months = [
   'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
 ];
 
-var monthsAbbr = [
-  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
-];
-
 module.exports = {
   months: function() {
     return months;
   },
 
   monthsAbbr: function() {
-    return monthsAbbr;
+    return this.months().map(function(month) {
+      return month.slice(0, 3);
+    });
   },
 
   years: function(from, to) {
