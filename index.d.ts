0f09191c39a6b4f0cc545b64f328c7b669f340db
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2023-05-22 14:03:21 -0300

    fix(types): `generateCalendar` interface #28

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,53 @@
 interface ConfigType {
-  months?: string[]
-  monthsAbbr?: string[]
+  months?: string[];
+  monthsAbbr?: string[];
 }
 
 interface CalendarType {
-  year: string
-  yearAbbr: string
-  month: string
-  monthAbbr: string
-  weekdays: string
-  weekdaysAbbr: string
-  days: number
-  firstWeekday: number
-  lastWeekday: number
-  calendar: number[][]
+  year: string;
+  yearAbbr: string;
+  month: string;
+  monthAbbr: string;
+  weekdays: string;
+  weekdaysAbbr: string;
+  days: number;
+  firstWeekday: number;
+  lastWeekday: number;
+  calendar: CalendarType[][];
 }
 
+type CalendarItem = {
+  date: Date;
+  day: number;
+  isInPrimaryMonth: boolean;
+  isInLastWeekOfPrimaryMonth: boolean;
+  index: {
+    day: number;
+    week: number;
+  };
+};
+
 interface CalendarJsType {
-  (config?: ConfigType): CalendarJsType
-  months(): string[]
-  monthsAbbr(): string[]
-  weekdays(): string[]
-  weekdaysAbbr(): string[]
-  years(from: number, to: number): string[]
-  yearsAbbr(from: number, to: number): string[]
-  generateCalendar(numberOfDays: number, firstWeekday: number, lastWeekday: number): number[][]
-  of(year: number, month: number): CalendarType
+  (config?: ConfigType): CalendarJsType;
+  months(): string[];
+  monthsAbbr(): string[];
+  weekdays(): string[];
+  weekdaysAbbr(): string[];
+  years(from: number, to: number): string[];
+  yearsAbbr(from: number, to: number): string[];
+  generateCalendar(
+    year: number,
+    month: number,
+    numberOfDays: number,
+    firstWeekday: number,
+    lastWeekday: number,
+    dayTransformer: (item: CalendarItem, callback: any) => CalendarItem,
+    defaultItem: CalendarType
+  ): CalendarType[][];
+  of(year: number, month: number): CalendarType;
 }
 
 declare module 'calendar-js' {
-  const CalendarJs: CalendarJsType
-  export default CalendarJs
+  const CalendarJs: CalendarJsType;
+  export default CalendarJs;
 }
